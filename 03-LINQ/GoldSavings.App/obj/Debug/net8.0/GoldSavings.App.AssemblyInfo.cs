64f8ec41a26124dbadfd7017ec3fb767462bdac8
Maxence CHUTAUX <darksas924@gmail.com>
2025-03-25 08:05:23 +0100

    push part of excellent part

--- 03-LINQ/GoldSavings.App/obj/Debug/net8.0/GoldSavings.App.AssemblyInfo.cs
+++ 03-LINQ/GoldSavings.App/obj/Debug/net8.0/GoldSavings.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoldSavings.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b377444e0c623a036acdb71af87422e93e22cf97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4903fe65034f6d17080e09af41a0c97564524fc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoldSavings.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoldSavings.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
